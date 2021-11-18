7b60493a4467bfa73cb1f31c4418346871232ec1
Your Name <you@example.com>
2021-11-18 22:45:21 +0800

    delete

--- tets/adder/target/debug/deps/adder-086d1f697f5f2650.d
+++
@@ -1,5 +0,0 @@
-/Users/kevin/go/src/Tobs/tets/adder/target/debug/deps/adder-086d1f697f5f2650: src/lib.rs
-
-/Users/kevin/go/src/Tobs/tets/adder/target/debug/deps/adder-086d1f697f5f2650.d: src/lib.rs
-
-src/lib.rs:
--- tets/adder/target/debug/deps/adder-a0128ccf54cf416d.d
+++
@@ -1,7 +0,0 @@
-/Users/kevin/go/src/Tobs/tets/adder/target/debug/deps/adder-a0128ccf54cf416d.rmeta: src/lib.rs
-
-/Users/kevin/go/src/Tobs/tets/adder/target/debug/deps/libadder-a0128ccf54cf416d.rlib: src/lib.rs
-
-/Users/kevin/go/src/Tobs/tets/adder/target/debug/deps/adder-a0128ccf54cf416d.d: src/lib.rs
-
-src/lib.rs:
