0b1ff4eb781d16f2bdb37431b493ee92b35272b3
Your Name <you@example.com>
2021-11-18 22:49:32 +0800

    简单使用erro方法
    Signed-off-by: Your Name <you@example.com>

--- learn_err2_0022/src/main.rs
+++ learn_err2_0022/src/main.rs
@@ -32,6 +32,15 @@ fn main() {
 //     }
 // }
 
+
+//fn read_username_from_file() -> Result<String, io::Error> {
+//    let mut f = File::open("hello.txt")?;
+//
+//    let mut s = String::new();
+//    f.read_to_string(&mut s)?;
+//    Ok(s)
+//}
+
 fn read_username_from_file() -> Result<String, io::Error> {
     let mut s = String::new();
     File::open("hello.txt")?.read_to_string(&mut s)?;
