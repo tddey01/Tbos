24dcd88fe4127518276fe494b08cf8a1165b5a62
Your Name <you@example.com>
2021-11-18 22:43:31 +0800

    error 传播错误方式

---
+++ tets/adder/target/debug/deps/adder-086d1f697f5f2650.d
@@ -0,0 +1,5 @@
+/Users/kevin/go/src/Tobs/tets/adder/target/debug/deps/adder-086d1f697f5f2650: src/lib.rs
+
+/Users/kevin/go/src/Tobs/tets/adder/target/debug/deps/adder-086d1f697f5f2650.d: src/lib.rs
+
+src/lib.rs:
---
+++ tets/adder/target/debug/deps/adder-a0128ccf54cf416d.d
@@ -0,0 +1,7 @@
+/Users/kevin/go/src/Tobs/tets/adder/target/debug/deps/adder-a0128ccf54cf416d.rmeta: src/lib.rs
+
+/Users/kevin/go/src/Tobs/tets/adder/target/debug/deps/libadder-a0128ccf54cf416d.rlib: src/lib.rs
+
+/Users/kevin/go/src/Tobs/tets/adder/target/debug/deps/adder-a0128ccf54cf416d.d: src/lib.rs
+
+src/lib.rs:
