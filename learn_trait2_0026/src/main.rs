998fa74efede7305cd7a7b1090807c2fe58eb6f0
Your Name <you@example.com>
2021-12-22 23:01:25 +0800

    Trait 使用trait bound的写法

---
+++ learn_trait2_0026/src/main.rs
@@ -0,0 +1,67 @@
+//1、trait_bound语法
+//2、指定多个trait bound
+//3、返回 trait的类型
+//fn print_information(item: impl GetInformation) { //直接作为参数的写法
+//fn print_information<T: GetInformation>(item: T){ //使用trait bound的写法
+//    println!("name = {}", item.get_name());
+//    println!("age = {}", item.get_age());
+//}
+
+trait GetName {
+    fn get_name(&self) -> &String;
+}
+
+trait GetAge {
+    fn get_age(&self) -> u32;
+}
+// 写法一
+// fn print_information<T: GetName + GetAge>(item: T) {
+//     //使用trait bound的写法
+//     println!("name {}", item.get_name());
+//     println!("age = {}", item.get_age());
+// }
+
+// 写法二
+fn print_information<T>(item: T)
+where
+    T: GetName + GetAge, // 约束方法
+{
+    println!("name {}", item.get_name());
+    println!("age = {}", item.get_age());
+}
+#[derive(Debug)]
+pub struct Student {
+    pub name: String,
+    pub age: u32,
+}
+
+impl GetName for Student {
+    fn get_name(&self) -> &String {
+        &self.name
+    }
+}
+
+impl GetAge for Student {
+    fn get_age(&self) -> u32 {
+        self.age
+    }
+}
+fn main() {
+    let s = Student {
+        name: "xiaoming".to_string(),
+        age: 10,
+    };
+    print_information(s);
+
+    let s = produce_item_with_age();
+
+    println!("Hello, world!");
+}
+
+fn produce_item_with_age() -> impl GetAge {
+    //返回值是一个特征 要求
+    Student {
+        name: String::from("xiaoming"),
+        age: 15,
+    }
+}
