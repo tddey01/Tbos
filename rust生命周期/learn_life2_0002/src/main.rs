2079f4010069cf5d7020b1c7973da0aa40accbb6
Your Name <you@example.com>
2022-01-03 23:00:51 +0800

    结构体中的生命周期的2

--- rust生命周期/learn_life2_0002/src/main.rs
+++ rust生命周期/learn_life2_0002/src/main.rs
@@ -9,10 +9,23 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     }
 }
 
+fn get_str<'a>(x: &'a str, _y: &str) -> &'a str {
+    x
+}
+
+// fn a_st<'a>(x: &'a str, y: &'a str) -> &'a str {  //error 错误例子
+//     let r = String::from("abc");
+//     r.as_str()
+// }
+
 fn main() {
     let s1 = String::from("abcde");
     let s2 = String::from("ab");
     let r = longest(s1.as_str(), s2.as_str());
     println!("r = {}", r);
+
+    let ss = get_str(s1.as_str(), s2.as_str());
+    // let ss2 = as_str(s1.as_str(), s2.as_str());
+    println!("ss = {}", ss);
     println!("Hello, world!");
 }
