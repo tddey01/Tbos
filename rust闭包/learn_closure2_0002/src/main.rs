69938cb4cd2e0b30a19515a9244d78153300dbe6
Your Name <you@example.com>
2022-01-18 21:42:11 +0800

    闭包

--- rust闭包/learn_closure2_0002/src/main.rs
+++ rust闭包/learn_closure2_0002/src/main.rs
@@ -1,23 +1,39 @@
 // 实现一个缓存 值处理第一次传入的值并保存
-
 struct Cacher<T>
 where
     T: Fn(u32) -> u32,
 {
     calcuation: T,
-    valueL: Option<u32>,
+    value: Option<u32>,
 }
 
-impl <T> Cacher<T>
+impl<T> Cacher<T>
 where
-T:Fn(u32) ->u32
+    T: Fn(u32) -> u32,
 {
-    fn new(calcuation:T)->Cacher<T>{
-        Cacher{
+    fn new(calcuation: T) -> Cacher<T> {
+        Cacher {
             calcuation,
+            value: None,
+        }
+    }
+    fn value(&mut self, rag: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calcuation)(rag);
+                self.value = Some(v);
+                v
+            }
         }
     }
 }
 fn main() {
+    let mut c = Cacher::new(|x| x + 1);
+    let v1 = c.value(1);
+    println!("{}", v1);
+
+    let  v2 = c.value(2);
+    println!("{}", v2);
     println!("Hello, world!");
 }
