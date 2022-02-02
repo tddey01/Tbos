969cdfd9d7fa16f3f6c0017a0abdb655d17364ca
Your Name <you@example.com>
2022-02-02 17:36:30 +0800

    解引用多态与可变性交互

--- Deref/learn_deref_0002/src/main.rs
+++ Deref/learn_deref_0002/src/main.rs
@@ -22,5 +22,16 @@ fn main() {
     assert_eq!(5, x);
     assert_eq!(5, *y); // 解引用
 
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m); // 将MyBox变为&String  在将 &String 的解引用，改变为字符串 slice， &str
+
     println!("Hello, world!");
 }
+
+fn hello(name: &str) {
+    println!("hello  {}", name);
+}
+//解引用多态与可变性交互：
+//（1）当T：Deref<Target=U>时，从&T到&U
+// (2)当T：DerefMut<Target=U>时，从&mut T 到&mut U
+//（3）当T：Deref<Target=U>时，从&mut T到&U
